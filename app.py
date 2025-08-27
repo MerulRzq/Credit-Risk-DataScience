dbe31253a5f6a21ff1041d6be81d0e4e3e994d65
Amirul <amrlrziq@gmail.com>
2025-08-28 02:47:52 +0800

    Commit 2 files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 model = joblib.load("extra_trees_credit_model.pkl")
-encoders = {col : joblib.load(f"{col}_encoder.pkl") for col in ["Sex", "Housing", "Saving accounts", "Checking account"]}
+encoders = {col : joblib.load(f"{col}_encoder.pkl") for col in ["Sex", "Housing", "Saving accounts", "Checking account", "Purpose"]}
 
 st.title("Credit Risk Prediction App")
 st.write("Enter applicant information to predict if the credit risk is good or bad")
